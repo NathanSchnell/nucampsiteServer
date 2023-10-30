459d8ac715b3b0f7108e18c1979d439555f11ab1
Nomad <schnell.nate@gmail.com>
2023-10-30 19:56:31 -0400

    updated basic auth

--- app.js
+++ app.js
@@ -10,18 +10,19 @@ const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const url = 'mongodb://0.0.0.0:27017/nucampsite';
+const url = "mongodb://0.0.0.0:27017/nucampsite";
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useFindAndModify: false,
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
-connect.then(() => console.log('Connected correctly to server'), 
-    err => console.log(err)
+connect.then(
+    () => console.log("Connected correctly to server"),
+    (err) => console.log(err)
 );
 
 const app = express();
@@ -35,28 +36,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-function auth(req,res,next) {
+function auth(req, res, next) {
     console.log(req.headers);
     const authHeader = req.headers.authorization;
-    if(!authHeader) {
-        const err = new Error('You are not authenticated');
-        res.setHeader('WWW-Authenticate', 'Basic');
+    if (!authHeader) {
+        const err = new Error("You are not authenticated!");
+        res.setHeader("WWW-Authenticate", "Basic");
         err.status = 401;
         return next(err);
     }
 
-    const auth = Buffer.from(authHeader.split('')[1], 'base64').toString().split(':');
+    const auth = Buffer.from(authHeader.split(" ")[1], "base64")
+        .toString()
+        .split(":");
     const user = auth[0];
     const pass = auth[1];
-    if(user === 'admin' && pass === 'password') {
-        return next(); //authorized
+    if (user === "admin" && pass === "password") {
+        return next(); // authorized
     } else {
-        const err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
+        const err = new Error("You are not authenticated!");
+        res.setHeader("WWW-Authenticate", "Basic");
         err.status = 401;
         return next(err);
     }
 }
+
 app.use(auth);
 app.use(express.static(path.join(__dirname, "public")));
 
